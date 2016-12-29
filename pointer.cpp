28a483c89ad6dfbe02ac8e7c95bd36aa5fc84f86
HI <mac@Hi-Sofia.Home>
2016-12-29 09:06:54 +0800

    Updated29/12

---
+++ pointer.cpp
@@ -0,0 +1,14 @@
+ #include <iostream>
+ using namespace std;
+
+ int main()
+        {
+                cout << "Hello world";
+                cout << "I'm a C++ program";
+                
+
+
+        }
+ 
+
+
